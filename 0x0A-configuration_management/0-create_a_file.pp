a7fbf3558d6d9d50fce5cca8d622d75b75518757
kelvin okoye <kelvinokoye413@gmail.com>
2024-03-09 05:54:51 +0000

    file resource

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,15 @@
-/*
-    Using Puppet, create a file in /tmp.
-    set the file ownership
-    set the file group
-    content = 'I love Puppet'
-*/
+# Using Puppet, create a file in /tmp.
+# set the file ownership
+# set the file group
+# content = 'I love Puppet'
 
+$file_path = /tmp/school
+
+file {$file_path:
+
+    ensure => present;
+    mode => '0744',
+    owner => 'www-data',
+    group => 'www-data',
+    content => 'I love Puppet',
+}
