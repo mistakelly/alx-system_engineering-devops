01a5bd246ed829658f5147556fc1ad897a6bdbb0
kelvin okoye <kelvinokoye413@gmail.com>
2024-03-09 06:02:10 +0000

    fix indentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,10 +2,9 @@
 # set the file ownership
 # set the file group
 # content = 'I love Puppet'
-
 file {'/tmp/school':
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 }
