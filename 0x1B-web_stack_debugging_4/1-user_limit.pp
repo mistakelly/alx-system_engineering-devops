73946267af6e59080ecb4ec9c8518ffb409c4e19
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-19 03:24:54 +0100

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-# OS configuration user limit
+#OS configuration user limit
 exec { 'Correct hard':
   command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
   provider => shell,
