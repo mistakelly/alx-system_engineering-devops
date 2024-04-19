e79c7d94729593beb937a7f090c897b48cccfe19
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-19 03:28:19 +0100

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,18 @@
-# Sky is the limit, let's bring that limit higher
-exec { 'file limit':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+# Puppet manifest to optimize Nginx configuration for handling load
+
+class nginx_config {
+    file { '/etc/nginx/nginx.conf':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('nginx/nginx.conf.erb'),
+        notify  => Service['nginx'],
+    }
+}
+
+service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Class['nginx_config'],
 }
